2f5c4ddd3d5dcd99f75f2e9dbdbccfa5f1043915
AndresGarciaLv <monsterreset38@gmail.com>
2025-03-08 23:45:31 -0500

    Add: Se agregaron nuevos assets y vistas de autenticación

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
+
     </div>
   </header>
 
